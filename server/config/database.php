1db74f92615437132f31fe2eb52227687ed3a8b7
Razvan Pop <pop.razvan03@yahoo.com>
2024-05-17 23:57:54 +0300

    refactoring

--- server/config/database.php
+++ server/config/database.php
@@ -1,12 +1,12 @@
 <?php
-    $env = parse_ini_file("../.env");
+$env = parse_ini_file("../../.env");
 
-    $host = $env["DATABASE_HOST"];
-    $username = $env["DATABASE_USERNAME"];
-    $password = $env["DATABASE_PASSWORD"];
-    $name = $env["DATABASE_NAME"];
+$servername = $env["DATABASE_SERVERNAME"];
+$username = $env["DATABASE_USERNAME"];
+$password = $env["DATABASE_PASSWORD"];
+$dbname = $env["DATABASE_NAME"];
 
-    $connection = mysqli_connect($host, $username, $password, $name);
-    if (! $connection) {
-        die("Could not connect to database. " . mysqli_connect_error());
-    }
+$connection = mysqli_connect($servername, $username, $password, $dbname);
+if (! $connection) {
+    die("Could not connect to database. " . mysqli_connect_error());
+}
