9599174bdb830d417c05e5665e43e843ff4e766d
Razvan Pop <pop.razvan03@yahoo.com>
2024-05-17 22:55:39 +0300

    deleted files from root

--- utils/create-tables.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE files (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    format_type VARCHAR(50) NOT NULL,
-    genre VARCHAR(50),
-    file_path VARCHAR(255) NOT NULL
-);
